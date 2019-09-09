90957f12cdbd32d0bc73a7c67affa7ea6aafebf4
IPAndreev <aip1990andreev@yandex.ru>
2019-09-09 23:19:42 +0300

    Change task

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -10,75 +10,88 @@ public class Main {
         System.out.println("Если Вам необходимо воспользоваться калькулятором - введите 1;");
         System.out.println("Если Вам необходим поиск максимального элемента в массиве - введите 2;\n");
         System.out.print("Ваш выбор: ");
+        boolean replay;
+        int choice;
+        do {
+            replay = true;
+            choice = scanner.nextInt();
 
-        int choice = scanner.nextInt();
+            if (choice == 1) {
+                boolean replay_1;
+                System.out.print("Введите первое число:");
+                double x = scanner.nextDouble();
+                System.out.print("Введите второе число:");
+                double y = scanner.nextDouble();
+                System.out.println("Доступные операции: ");
+                System.out.print("+\n-\n*\n/\n");
 
-        if (choice ==1){
-            System.out.print("Введите первое число:");
-            double x = scanner.nextDouble();
-            System.out.print("Введите второе число:");
-            double y = scanner.nextDouble();
-            System.out.println("Доступные операции: ");
-            System.out.print("+\n-\n*\n/\n");
+                System.out.print("Выберите операцию: ");
+                do {
 
-            System.out.print("Выберите операцию: ");
-            char z = scanner.next().charAt(0);
+                    replay_1 = true;
+                    char z = scanner.next().charAt(0);
 
-            switch (z) {
-                case '+':
-                    System.out.printf(x + " + " + y + " = %.4f", (x + y));
-                    break;
-                case '-':
-                    System.out.printf(x + " - " + y + " = %.4f", (x - y));
-                    break;
-                case '*':
-                    System.out.printf(x + " * " + y + " = %.4f", (x * y));
-                    break;
-                case '/':
-                    if (y == 0) {
-                        System.out.println("На 0 делить нельзя");
-                    } else {
-                        System.out.printf(x + " / " + y + " = %.4f", (x / y));
-                        break;
+                    switch (z) {
+                        case '+':
+                            System.out.printf(x + " + " + y + " = %.4f", (x + y));
+                            break;
+                        case '-':
+                            System.out.printf(x + " - " + y + " = %.4f", (x - y));
+                            break;
+                        case '*':
+                            System.out.printf(x + " * " + y + " = %.4f", (x * y));
+                            break;
+                        case '/':
+                            if (y == 0) {
+                                System.out.println("На 0 делить нельзя");
+                                break;
+                            } else {
+                                System.out.printf(x + " / " + y + " = %.4f", (x / y));
+                                break;
+                            }
+                        default:
+                            replay_1 = false;
+                            System.out.println("Такой операции нет. Повторите ввод:");
                     }
-                default:
-                    System.out.println("Неверная операция");
-            }
 
-        }
-        else if (choice == 2){
-            System.out.println("Введите размер массива и нажмите Enter: ");
+                } while (replay_1 == false);
+            } else if (choice == 2) {
+                System.out.println("Введите размер массива и нажмите Enter: ");
 
-            int x = scanner.nextInt();
+                int x = scanner.nextInt();
 
-            try {
-                x = Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-
-            String array[] = new String[x];
-
-            for (int i = 0; i < array.length; i++) {
-                System.out.println("Введите " + (i + 1) + "-е слово и нажмите Enter: ");
-                array[i] = scanner.nextLine();
-            }
-
-            int y = 0;
-
-            for(int i=0;i<array.length;i++){
-                if(array[i].length()>y){
-                    y = array[i].length();
+                try {
+                    x = Integer.parseInt(scanner.nextLine());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
                 }
-            }
-            for(int i=0;i<array.length;i++){
-                if(array[i].length()==y){
-                    System.out.println("Слово с наибольшей длиной  " + '"' + array[i] + '"' + "  и его длина составляет " + y + " символа(ов)" );
+
+                String array[] = new String[x];
+
+                for (int i = 0; i < array.length; i++) {
+                    System.out.println("Введите " + (i + 1) + "-е слово и нажмите Enter: ");
+                    array[i] = scanner.nextLine();
                 }
+
+                int y = 0;
+
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i].length() > y) {
+                        y = array[i].length();
+                    }
+                }
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i].length() == y) {
+                        System.out.println("Слово с наибольшей длиной  " + '"' + array[i] + '"' + "  и его длина составляет " + y + " символа(ов)");
+                    }
+                }
+            } else {
+                replay = false;
+                System.out.println("Некорректный выбор. Повторите ввод");
+                System.out.println("Если Вам необходимо воспользоваться калькулятором - введите 1;");
+                System.out.println("Если Вам необходим поиск максимального элемента в массиве - введите 2;\n");
+                System.out.print("Ваш выбор: ");
             }
-        }
-        else {
-            System.out.println("Возможно вы ошиблись, повторите ввод.");
-        }
+        } while (replay == false);
     }
 }
